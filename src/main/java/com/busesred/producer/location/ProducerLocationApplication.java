ce61936bdda2c67c2b4bea5889f7c7bd29a33569
Diego Andrés Muñoz Morales <86175879+DimmoX@users.noreply.github.com>
2026-02-15 03:00:22 -0300

    feat: configurar aplicación y conexión RabbitMQ
    - Agregar application.yml con configuración de RabbitMQ
    - Agregar punto de entrada ProducerLocationApplication

---
+++ src/main/java/com/busesred/producer/location/ProducerLocationApplication.java
@@ -0,0 +1,11 @@
+package com.busesred.producer.location;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerLocationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerLocationApplication.class, args);
+    }
+}
