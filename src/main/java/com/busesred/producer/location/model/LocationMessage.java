2cd9cc9a81a467f01c05232faf33c5f2884c3e86
Diego Andrés Muñoz Morales <86175879+DimmoX@users.noreply.github.com>
2026-02-16 02:30:44 -0300

    feat: JSON español snake_case y config producción
    - @JsonProperty en LocationMessage (id_bus, latitud, longitud, etc.)
    - Respuestas controller en español
    - Dockerfile actualizado a Java 21

--- src/main/java/com/busesred/producer/location/model/LocationMessage.java
+++ src/main/java/com/busesred/producer/location/model/LocationMessage.java
@@ -3,6 +3,7 @@ package com.busesred.producer.location.model;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,10 +12,22 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LocationMessage implements Serializable {
+
+    @JsonProperty("id_bus")
     private String busId;
+
+    @JsonProperty("latitud")
     private Double latitude;
+
+    @JsonProperty("longitud")
     private Double longitude;
+
+    @JsonProperty("marca_tiempo")
     private LocalDateTime timestamp;
+
+    @JsonProperty("ruta")
     private String route;
+
+    @JsonProperty("velocidad")
     private Double speed;
 }
